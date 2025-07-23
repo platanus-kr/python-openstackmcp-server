4cc5a6af94252b52f2039f2dbaa20e4d776a1ee7
SeungJu Baek <halucinor0@gmail.com>
2025-07-23 20:21:44 +0900

    feat : mcp init (#1)
    mcp server init

---
+++ src/openstack_mcp_server/__main__.py
@@ -0,0 +1,3 @@
+from openstack_mcp_server import main
+
+main()
---
+++ src/openstack_mcp_server/config.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+# Transport protocol
+MCP_TRANSPORT = os.environ.get("TRANSPORT", "stdio")
+
+# Application paths
+BASE_DIR = Path(__file__).parent.parent.parent
