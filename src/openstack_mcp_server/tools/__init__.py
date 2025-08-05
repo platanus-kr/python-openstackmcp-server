3e0b1e5d28425c73853aa476ea670a56a714996a
김다경(Da Gyeong Kim) <65109465+S0okJu@users.noreply.github.com>
2025-08-06 00:22:37 +0900

    feat: Add get regions and create region tool  (#18)
    * feat(keystone): implement get-regions tool (#14)
    
    - Add get_regions tool
    - Add get_regions test code

--- src/openstack_mcp_server/tools/__init__.py
+++ src/openstack_mcp_server/tools/__init__.py
@@ -7,6 +7,8 @@ def register_tool(mcp: FastMCP):
     """
     from .glance_tools import GlanceTools
     from .nova_tools import NovaTools
+    from .keystone_tools import KeystoneTools
 
     NovaTools().register_tools(mcp)
     GlanceTools().register_tools(mcp)
+    KeystoneTools().register_tools(mcp)
---
+++ src/openstack_mcp_server/tools/keystone_tools.py
@@ -0,0 +1,52 @@
+from fastmcp import FastMCP
+from pydantic import BaseModel
+from .base import get_openstack_conn
+
+# NOTE: In openstacksdk, all of the fields are optional.
+# In this case, we are only using description field as optional.
+class Region(BaseModel):
+    id: str
+    description: str | None = None
+
+class KeystoneTools:
+    """
+    A class to encapsulate Keystone-related tools and utilities.
+    """
+
+    def register_tools(self, mcp: FastMCP):
+        """
+        Register Keystone-related tools with the FastMCP instance.
+        """
+
+        mcp.tool()(self.get_regions)
+        mcp.tool()(self.create_region)
+
+    def get_regions(self) -> list[Region]:
+        """
+        Get the list of Keystone regions.
+        
+        :return: A list of Region objects representing the regions.
+        """
+        conn = get_openstack_conn()
+
+        region_list = []
+        for region in conn.identity.regions():
+            region_list.append(Region(id=region.id, description=region.description))
+            
+        return region_list
+    
+    def create_region(self, id: str, description: str | None = None) -> Region:
+        """
+        Create a new region.
+        
+        :param id: The ID of the region.
+        :param description: The description of the region. It can be None.
+        
+        :return: The created Region object.
+        """
+        conn = get_openstack_conn()
+
+        region = conn.identity.create_region(id=id, description=description)
+
+        return Region(id=region.id, description=region.description)
+    
