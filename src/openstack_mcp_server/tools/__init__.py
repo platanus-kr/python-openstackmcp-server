86044b64dcb0b304458a0e1070bec873cc552a9a
platanus-kr <platanus.kr@gmail.com>
2025-08-06 21:12:43 +0900

    chore(neutron): code linting

--- src/openstack_mcp_server/tools/__init__.py
+++ src/openstack_mcp_server/tools/__init__.py
@@ -6,9 +6,9 @@ def register_tool(mcp: FastMCP):
     Register Openstack MCP tools.
     """
     from .glance_tools import GlanceTools
-    from .nova_tools import NovaTools
     from .keystone_tools import KeystoneTools
     from .neutron_tools import NeutronTools
+    from .nova_tools import NovaTools
 
     NovaTools().register_tools(mcp)
     GlanceTools().register_tools(mcp)
